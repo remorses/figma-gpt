bf93a2d641fc07ff3f0a1acc0c30d8a6b1f4d6fa
remorses <beats.by.morse@gmail.com>
2023-03-27 15:13:04 +0200

    __figma-gpt/src:

---
+++ __figma-gpt/src/App.tsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+export default function App() {
+    return (
+        <main class='App mt-10 mx-auto max-w-7xl p-12'>
+            <h1 class='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
+                <span>Preact</span>
+                <span> + </span>
+                <span class='text-indigo-600'>TailwindCSS</span>
+            </h1>
+        </main>
+    )
+}
