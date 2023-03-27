e2af9058eaf521ae1ac76f5f3b5f0c0c73a7fcd1
remorses <beats.by.morse@gmail.com>
2023-03-27 15:11:11 +0200

    vite works

--- __figma-gpt/vite.backend.js
+++ __figma-gpt/vite.backend.js
@@ -6,18 +6,15 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        viteSingleFile(), //
         // preact({ prefreshEnabled: false }),
     ],
     build: {
         emptyOutDir: false,
-        target: 'esnext',
-        assetsInlineLimit: 100000000,
-        chunkSizeWarningLimit: 100000000,
-        cssCodeSplit: false,
-        brotliSize: false,
-        rollupOptions: {
-            inlineDynamicImports: false,
+        lib: {
+            entry: path.resolve(__dirname, 'src/backend.ts'),
+            formats: ['iife'],
+            name: 'backend',
+            fileName: 'backend',
         },
     },
 })
